90596987136a6ee04e7c611156f6895b7a20c056
Bharat K. Premkumar <55843416+BharatPremkumar@users.noreply.github.com>
2023-07-18 22:07:05 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const btn = document.getElementById("btn");
+const btn2 = document.getElementById("genInfo");
+const btn3 = document.getElementById("close");
+const modal = document.getElementById("myModal");
+let count = 0;
+
+let img = new Image();
+img.src = "image/oslo.jpg";
+img.style.width = "80%";
+img.style.height = "50%";
+img.style.padding = "10%";
+document.getElementById("osloIMG").appendChild(img);
+img.style.display = "none";
+
+btn.addEventListener("click", addImg);
+btn2.addEventListener("click", addModule);
+btn3.addEventListener("click", closeModal);
+
+function addImg() {
+  count++;
+  if (count % 2 == 0) {
+    btn.innerHTML = "View oslo locations";
+    img.style.display = "none";
+  }
+  if (count % 2 != 0) {
+    btn.innerHTML = "Close image";
+    img.style.display = "block";
+  }
+}
+
+function addModule() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
