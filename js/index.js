a428cd88e3b994760e997be0552101e6f83b2715
Bharat K. Premkumar <55843416+BharatPremkumar@users.noreply.github.com>
2023-07-21 22:33:15 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,32 +2,11 @@ const btn = document.getElementById("btn");
 const btn2 = document.getElementById("genInfo");
 const btn3 = document.getElementById("close");
 const modal = document.getElementById("myModal");
-let count = 0;
+const pEl = document.getElementById("osloLocation");
 
-let img = new Image();
-img.src = "image/oslo.jpg";
-img.style.width = "80%";
-img.style.height = "50%";
-img.style.padding = "10%";
-img.style.borderRadius = "10px";
-document.getElementById("osloIMG").appendChild(img);
-img.style.display = "none";
-
-btn.addEventListener("click", addImg);
+const form = document.getElementById("myform");
 btn2.addEventListener("click", addModule);
 btn3.addEventListener("click", closeModal);
-
-function addImg() {
-  count++;
-  if (count % 2 == 0) {
-    btn.innerHTML = "View oslo locations";
-    img.style.display = "none";
-  }
-  if (count % 2 != 0) {
-    btn.innerHTML = "Close image";
-    img.style.display = "block";
-  }
-}
 
 function addModule() {
   modal.style.display = "block";
@@ -36,3 +15,94 @@ function addModule() {
 function closeModal() {
   modal.style.display = "none";
 }
+
+const locations = [
+  { id: 1, name: "tåsen senter" },
+  { id: 2, name: "gunerius" },
+  { id: 3, name: "grunerløkken" },
+  { id: 4, name: "St.hanshaugen" },
+  { id: 5, name: "kronen" },
+  { id: 6, name: "holtet" },
+  { id: 7, name: "nordstrand" },
+  { id: 8, name: "bryn" },
+  { id: 10, name: "tveita" },
+  { id: 11, name: "nittedal" },
+  { id: 12, name: "nydalen" },
+  { id: 13, name: "hagan" },
+  { id: 14, name: "haugerud" },
+  { id: 15, name: "skøyen" },
+  { id: 16, name: "romsås" },
+  { id: 17, name: "st.hanshaugen senter" },
+  { id: 18, name: "løren" },
+  { id: 19, name: "romerike helsebygg" },
+  { id: 21, name: "lamberseter" },
+  { id: 22, name: "storgata" },
+  { id: 23, name: "stovner" },
+  { id: 24, name: "frogner" },
+  { id: 25, name: "ensjø" },
+  { id: 26, name: "åråsen lillestrøm" },
+  { id: 27, name: "kiellandshus" },
+  { id: 28, name: "hasle linje" },
+  { id: 29, name: "eiksmarka" },
+  { id: 30, name: "lilleaker" },
+  { id: 31, name: "metro" },
+  { id: 32, name: "ringnes park" },
+  { id: 33, name: "ikke oppgitt i liste" },
+  { id: 34, name: "carl berner" },
+  { id: 35, name: "ikke oppgitt i liste" },
+  { id: 36, name: "grønland" },
+  { id: 37, name: "grorud" },
+  { id: 38, name: "kalbakken" },
+  { id: 39, name: "hv" },
+  { id: 40, name: "karlsrud" },
+  { id: 41, name: "kanalveien" },
+  { id: 42, name: "linderud" },
+  { id: 43, name: "lillo" },
+  { id: 44, name: "harbitz torg" },
+  { id: 45, name: "solheim" },
+  { id: 46, name: "torshov" },
+  { id: 47, name: "ikke oppgitt i liste" },
+  { id: 48, name: "nordstjernen" },
+  { id: 49, name: "ulven" },
+  { id: 50, name: "holmlia" },
+  { id: 51, name: "liseapotekene lillestrøm" },
+  { id: 52, name: "carl berner torg" },
+  { id: 53, name: "fjellhamar" },
+  { id: 54, name: "skjettentoppen" },
+  { id: 55, name: "røa" },
+  { id: 56, name: "ullevaal stadion" },
+  { id: 57, name: "vinderen" },
+];
+
+function findIdByLocation(inputValue) {
+  for (const item of locations) {
+    if (item.name === inputValue) {
+      return item.id;
+    }
+  }
+  return null; // Return null if no match is found
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const locationInput = document.getElementById("location");
+  const locationValue = locationInput.value.toLowerCase();
+  console.log(locationValue);
+
+  const foundId = findIdByLocation(locationValue);
+  if (foundId !== null) {
+    pEl.style.color = "#1b6613";
+    pEl.innerHTML = `${locationInput.value} is on track: ${foundId}`;
+  } else if (
+    locationInput === "røa" ||
+    locationInput === "ullevaal stadion" ||
+    locationInput === "vinderen"
+  ) {
+    pEl.style.color = "red";
+    pEl.innerHTML = `${locationInput.value} is track is the same location as where 190T is sorted: `;
+  } else {
+    pEl.style.color = "red";
+    pEl.innerHTML =
+      "Location does not exist. Please check for spelling errors.";
+  }
+});
